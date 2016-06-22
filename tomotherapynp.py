9f999e8d7a059f68575e2ed3318475c215c314a2
Wilmer E. Henao <wilmer@umich.edu>
2016-06-22 10:53:36 -0500

    Added matrix multiplication wrapper

--- tomotherapynp.py
+++ tomotherapynp.py
@@ -14,7 +14,7 @@ import gurobipy as gp
 import numpy as np
 import scipy.sparse as sps
 import os
-
+## Function that reads the files produced by Weiguo
 def getvector(necfile,dtype):
     with open(necfile, 'rb') as f:
         try:
@@ -23,14 +23,30 @@ def getvector(necfile,dtype):
             f.close()
     return(data)
 
+## This function creates a sparse matrix
+def sparseWrapper(bixels, voxels, Dijs, mask, totalbeamlets, totalVoxels):
+    D = sps.csc_matrix((Dijs, (bixels, voxels)), shape=(totalbeamlets, totalVoxels))
+    return(D)
+
+## Number of beamlets in each gantry. Usually 64 but Weiguo uses 80
+beamletsInGantry = 80
+## Number of control points (every 2 degrees)
+controlPoints = 178
+## Total number of beamlets
+totalbeamlets = controlPoints * beamletsInGantry
+## Dimensions of the 2-Dimensional screen
+dimX = 256
+dimY = 256
+## Total number of voxels in the phantom
+totalVoxels = dimX * dimY
+
 bixels = getvector('data\\Bixels_out.bin', np.int32)
 voxels = getvector('data\\Voxels_out.bin', np.int32)
 Dijs = getvector('data\\Dijs_out.bin', np.float32)
 mask = getvector('data\\optmask.img', np.int32)
-print(len(mask))
 
-D = sps.csc_matrix((Dijs,(bixels,voxels)),shape=(14240,65536))
-#
+D = sparseWrapper(bixels, voxels, Dijs, mask, totalbeamlets, totalVoxels)
+
 # cwd = os.getcwd()  # Get the current working directory (cwd)
 # files = os.listdir(cwd)  # Get all the files in that directory
 # print(cwd, files)
