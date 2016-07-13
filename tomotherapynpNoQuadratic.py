077bc7f4af4e000c030c226dad86ae757133d73f
Wilmer E. Henao <wilmer@umich.edu>
2016-07-13 15:31:53 -0500

    Added a callback

--- tomotherapynpNoQuadratic.py
+++ tomotherapynpNoQuadratic.py
@@ -26,6 +26,82 @@ import math
 from scipy.stats import describe
 
 numcores = 4
+
+def mycallback(model, where):
+    if where == GRB.Callback.POLLING:
+        # Ignore polling callback
+        pass
+    elif where == GRB.Callback.PRESOLVE:
+        # Presolve callback
+        cdels = model.cbGet(GRB.Callback.PRE_COLDEL)
+        rdels = model.cbGet(GRB.Callback.PRE_ROWDEL)
+        if cdels or rdels:
+            print('%d columns and %d rows are removed' % (cdels, rdels))
+    elif where == GRB.Callback.SIMPLEX:
+        # Simplex callback
+        print('simplex')
+        itcnt = model.cbGet(GRB.Callback.SPX_ITRCNT)
+        if itcnt - model._lastiter >= 100:
+            model._lastiter = itcnt
+            obj = model.cbGet(GRB.Callback.SPX_OBJVAL)
+            ispert = model.cbGet(GRB.Callback.SPX_ISPERT)
+            pinf = model.cbGet(GRB.Callback.SPX_PRIMINF)
+            dinf = model.cbGet(GRB.Callback.SPX_DUALINF)
+            if ispert == 0:
+                ch = ' '
+            elif ispert == 1:
+                ch = 'S'
+            else:
+                ch = 'P'
+            print('%d %g%s %g %g' % (int(itcnt), obj, ch, pinf, dinf))
+    elif where == GRB.Callback.MIP:
+        # General MIP callback
+        nodecnt = model.cbGet(GRB.Callback.MIP_NODCNT)
+        objbst = model.cbGet(GRB.Callback.MIP_OBJBST)
+        objbnd = model.cbGet(GRB.Callback.MIP_OBJBND)
+        solcnt = model.cbGet(GRB.Callback.MIP_SOLCNT)
+        if nodecnt - model._lastnode >= 100:
+            model._lastnode = nodecnt
+            actnodes = model.cbGet(GRB.Callback.MIP_NODLFT)
+            itcnt = model.cbGet(GRB.Callback.MIP_ITRCNT)
+            cutcnt = model.cbGet(GRB.Callback.MIP_CUTCNT)
+            print('%d %d %d %g %g %d %d' % (nodecnt, actnodes, \
+                  itcnt, objbst, objbnd, solcnt, cutcnt))
+        if abs(objbst - objbnd) < 0.1 * (1.0 + abs(objbst)):
+            print('Stop early - 10% gap achieved')
+            model.terminate()
+        if nodecnt >= 10000 and solcnt:
+            print('Stop early - 10000 nodes explored')
+            model.terminate()
+    elif where == GRB.Callback.MIPSOL:
+        # MIP solution callback
+        nodecnt = model.cbGet(GRB.Callback.MIPSOL_NODCNT)
+        obj = model.cbGet(GRB.Callback.MIPSOL_OBJ)
+        solcnt = model.cbGet(GRB.Callback.MIPSOL_SOLCNT)
+        x = model.cbGetSolution(model.getVars())
+        print('**** New solution at node %d, obj %g, sol %d, ' \
+              'x[0] = %g ****' % (nodecnt, obj, solcnt, x[0]))
+    elif where == GRB.Callback.MIPNODE:
+        # MIP node callback
+        print('**** New node ****')
+        if model.cbGet(GRB.Callback.MIPNODE_STATUS) == GRB.Status.OPTIMAL:
+            x = model.cbGetNodeRel(model.getVars())
+            model.cbSetSolution(model.getVars(), x)
+    elif where == GRB.Callback.BARRIER:
+        # Barrier callback
+        itcnt = model.cbGet(GRB.Callback.BARRIER_ITRCNT)
+        primobj = model.cbGet(GRB.Callback.BARRIER_PRIMOBJ)
+        dualobj = model.cbGet(GRB.Callback.BARRIER_DUALOBJ)
+        priminf = model.cbGet(GRB.Callback.BARRIER_PRIMINF)
+        dualinf = model.cbGet(GRB.Callback.BARRIER_DUALINF)
+        cmpl = model.cbGet(GRB.Callback.BARRIER_COMPL)
+        print('%d %g %g %g %g %g' % (itcnt, primobj, dualobj, \
+              priminf, dualinf, cmpl))
+    elif where == GRB.Callback.MESSAGE:
+        # Message callback
+        msg = model.cbGet(GRB.Callback.MSG_STRING)
+        model._logfile.write(msg)
+
 ## Class definition of the gurobi object that handles creation and execution of the model
 # Original template from Troy Long.
 class tomotherapyNP(object):
@@ -39,6 +115,7 @@ class tomotherapyNP(object):
         self.mod.params.MIPFocus = 3
         self.mod.params.PreSparsify = 1
         self.mod.params.Presolve = 1
+        self.mod.params.MIPGapAbs = 0.05
         #self.mod.params.TimeLimit = 4.0 # Time limit in seconds
         print('done')
         print('Building main decision variables (dose, binaries).')
@@ -236,7 +313,7 @@ class tomotherapyNP(object):
 
         self.mod.setObjective(objexpr, grb.GRB.MAXIMIZE) #1.0 expresses minimization. It is the model sense.
         self.mod.update()
-        self.mod.optimize()
+        self.mod.optimize(mycallback())
         print('done')
 
     def outputSolution(self):
@@ -378,7 +455,7 @@ class tomodata:
         print('dijs length: ', len(self.Dijs))
         print('mask length: ', len(self.mask))
         print('remove all values of zeroes... and double checking')
-        self.sizereduction(200)
+        self.sizereduction(500)
         locats = np.where(0 == self.mask)[0]
         self.mask = np.delete(self.mask, locats)
         print('locats: ', len(locats))
