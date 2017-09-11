89a0013608001adee7b055d3c8140578c6187aa8
Wilmer E. Henao <wilmer@umich.edu>
2017-09-11 16:58:52 -0400

    Added 2 new files

--- data/dij/img_structure_plotting.py
+++ data/dij/img_structure_plotting.py
@@ -4,22 +4,19 @@
 # In[1]:
 
 
+import matplotlib as mpl
+mpl.use('TkAgg')
 import matplotlib.colors as col
 import matplotlib.pyplot as plt
 
-
 import numpy as np
-# get_ipython().magic(u'matplotlib inline')
 
 import scipy.sparse as sps
 
-
-
 # In[2]:
 
-
 base_dir = '123456 -Helical Gyn/'
-# base_dir = 'prostate/'
+base_dir = 'prostate/'
 
 # img_filename = 'roimask.img'
 # header_filename = 'roimask.header'
@@ -27,8 +24,6 @@ img_filename = 'samplemask.img'
 header_filename = 'samplemask.header'
 struct_img_filename = 'roimask.img'
 struct_img_header = 'roimask.header'
-
-
 
 # In[3]:
 def getvector(necfile,dtype):
@@ -83,7 +78,6 @@ def plot_structure(img,struct_id_list,zslices,printSliceCount=False, plotstuff=T
     
         img_struct[np.where(img_arr & 2**(s-1))] = s
 
-                  
     count = 0
     if isinstance(zslices,int):
         num_slices = 1
@@ -99,9 +93,7 @@ def plot_structure(img,struct_id_list,zslices,printSliceCount=False, plotstuff=T
                 plt.imshow(img_struct[:,:,z],cmap='gray',interpolation='nearest')
                 plt.show()
     if printSliceCount:
-        print 'Structure(s) {} is in {} of {} plotted slices'.format(str(struct_id_list),count,num_slices)
-        
-        
+        print('Structure(s)' + str(count) + ' is in ' + str(num) + ' of ' + str(slices) + ' plotted slices')
 
 # plot_structure(img_arr,np.array([1,3,7,10]),np.arange(60,65),printSliceCount=True,plotstuff=True)
 
@@ -115,43 +107,48 @@ dim_xyz = get_dim(base_dir, header_filename)
 img_arr = get_img_array(base_dir, img_filename, dim_xyz, dtype=dtype)
 # get structure file
 struct_img_arr = get_img_array(base_dir, struct_img_filename, dim_xyz, dtype=dtype)
-print struct_img_arr.shape, np.count_nonzero(struct_img_arr), 'full struct img shape, number nonzero vox'
+print(np.unique(struct_img_arr))
+print(struct_img_arr.shape, np.count_nonzero(struct_img_arr), 'full struct img shape, number nonzero vox')
 
 struct_id_list = [i for i in range(1,22)]
-# struct_id_list = [i for i in range(1,2)]
 img_struct = get_structure_mask(struct_id_list, struct_img_arr)
 
 sub_sampled_img_struct = get_subsampled_mask(img_struct, img_arr)
 
-print sub_sampled_img_struct.shape, np.count_nonzero(sub_sampled_img_struct), 'subsampled img shape, number nonzero vox'
-print struct_img_arr.shape, np.count_nonzero(struct_img_arr), 'full struct img shape, number nonzero vox'
+print(sub_sampled_img_struct.shape, np.count_nonzero(sub_sampled_img_struct), 'subsampled img shape, number nonzero vox')
+print(struct_img_arr.shape, np.count_nonzero(struct_img_arr), 'full struct img shape, number nonzero vox')
 struct_img_arr
 bixels = getvector(base_dir + 'dij/Bixels_out.bin', np.int32)
 voxels = getvector(base_dir + 'dij/Voxels_out.bin', np.int32)
 Dijs = getvector(base_dir + 'dij/Dijs_out.bin', np.float32)
-print bixels.shape, bixels.min(), bixels.max(), 'bix shape, bix min, bix max'
-print voxels.shape, voxels.min(), voxels.max(), 'vox'
-print Dijs.shape, Dijs.min(), Dijs.max(), 'dij'
-print (np.unique(voxels)).size, 'num unique voxels'
+print(bixels.shape, bixels.min(), bixels.max(), 'bix shape, bix min, bix max')
+print(voxels.shape, voxels.min(), voxels.max(), 'vox')
+print(Dijs.shape, Dijs.min(), Dijs.max(), 'dij')
+print((np.unique(voxels)).size, 'num unique voxels')
 
 flatNP = sub_sampled_img_struct.flatten(order='F')
-print np.unique((flatNP[np.unique(voxels)])), (flatNP[np.unique(voxels)]).size, 'vox getting radiation'
-print np.sum(sub_sampled_img_struct[sub_sampled_img_struct>0]), sub_sampled_img_struct.shape, sub_sampled_img_struct.size, 'subsampled body mask vox count, img shape, img size'
+print(np.unique((flatNP[np.unique(voxels)])), (flatNP[np.unique(voxels)]).size, 'vox getting radiation')
+print(np.sum(sub_sampled_img_struct[sub_sampled_img_struct>0]), sub_sampled_img_struct.shape, sub_sampled_img_struct.size, 'subsampled body mask vox count, img shape, img size')
+
+import matplotlib as mpl
 
 
+print(img_arr[img_arr>0].size)
 
-print img_arr[img_arr>0].size
+#plt.imshow(img_arr[:,:,80],cmap='gray',interpolation='nearest')
+#plt.show()
+flatNP = sub_sampled_img_struct.flatten(order='F')
+countflatNP = np.array([i if 0.1 < flatNP[i] else 0 for i in range(len(flatNP)) ])
 
-# plt.imshow(img_arr[:,:,80],cmap='gray',interpolation='nearest')
-# plt.show()
-
-plt.imshow(sub_sampled_img_struct[:,:,80],cmap='gray',interpolation='nearest')
+lp = np.in1d(countflatNP, voxels)
+print(np.count_nonzero(lp), np.count_nonzero(countflatNP), 'voxels taken into account and total voxels')
+plt.imshow(sub_sampled_img_struct[:,:,80],cmap='autumn',interpolation='none')
 plt.show()
-plotter = np.zeros_like(struct_img_arr) 
+plotter = np.zeros_like(struct_img_arr)
 plotter[np.where(img_arr)] = struct_img_arr[np.where(img_arr)]
 
-print np.count_nonzero(img_struct[np.where(img_arr)]), 'number nonzero where sampled'
-print plotter.shape
+print(np.count_nonzero(img_struct[np.where(img_arr)]), 'number nonzero where sampled')
+print(plotter.shape)
 plt.imshow(plotter[:,:,80],cmap='gray',interpolation='nearest')
 plt.show()
 
@@ -161,4 +158,4 @@ plt.show()
 plt.imshow(struct_img_arr[:,:,80],cmap='gray',interpolation='nearest')
 plt.show()
 
-# plot_structure(struct_img_arr, struct_id_list, np.array([80,82]))
+#plot_structure(struct_img_arr, struct_id_list, np.array([80,82]))
